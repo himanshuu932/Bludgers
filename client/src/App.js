b7bae662afae46aff0532a8b39829181f813ad82
himanshuu932 <himanshuu932@gmail.com>
2025-05-10 00:59:19 +0530

    added page

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ContactPage from './components/ContactUs';
+function App() {
+  return (
+    <div className="App">
+    <ContactPage/>
+    </div>
+  );
+}
+
+export default App;
